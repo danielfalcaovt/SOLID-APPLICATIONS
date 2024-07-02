59809a8b8993febf2bd112e2e1bee9e7668992b1
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-02 16:53:45 -0300

    feat: ensure return 400 if no email or password is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+
+import MissingParamError from "../errors/missing-param-error"
+
 export class SignUpController {
     handle(httpRequest: any): any {
+        const requiredParameters = ['username', 'email', 'password', 'confirmPassword']
+        for (const pos of requiredParameters) {
+            if (!httpRequest.body[pos]) {
+                return new Error("missing param")
+            }
+        }
         return {
             statusCode: 400
         }

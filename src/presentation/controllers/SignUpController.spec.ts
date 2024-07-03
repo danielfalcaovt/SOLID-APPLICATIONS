749ed7b53196cc68cb7f3152fea1375bef3f0073
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-03 13:30:44 -0300

    refactor: move sut creaetion to a helper factory method

--- src/presentation/controllers/SignUpController.spec.ts
+++ src/presentation/controllers/SignUpController.spec.ts
@@ -2,9 +2,13 @@ import MissingParamError from "../errors/missing-param-error"
 import { HttpRequest } from "../protocols/http-protocols"
 import { SignUpController } from "./signup"
 
+const makeSut = () => {
+    return new SignUpController()
+}
+
 describe('SignUp Controller', () => {
     test('Should return 400 if no name was provided', () => {
-        const sut = new SignUpController()
+        const sut = makeSut()
         const httpRequest = {
             body: {
                 email: 'any_mail',
@@ -19,7 +23,7 @@ describe('SignUp Controller', () => {
     })
 
     test('Should return 400 if no email was provided.', () => {
-        const sut = new SignUpController()
+        const sut = makeSut()
         const httpRequest = {
             body: {
                 name: 'any_name',
@@ -34,7 +38,7 @@ describe('SignUp Controller', () => {
     })
     
     test('Should return 400 if no password was provided', () => {
-        const sut = new SignUpController()
+        const sut = makeSut()
         const httpRequest: HttpRequest = {
             body: {
                 name: 'any_name',
@@ -46,4 +50,32 @@ describe('SignUp Controller', () => {
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new MissingParamError('password'))
     })
+    test('Should return 400 if no confirmation password was provided', () => {
+        const sut = makeSut()
+        const httpRequest: HttpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'any_password',
+                password: 'any_password'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError('confirmPassword'))
+    })
+    test('Should return 400 if an invalid email was provided', () => {
+        const sut = makeSut()
+        const httpRequest: HttpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'any_password',
+                password: 'any_password',
+                confirmPassword: 'any_password'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new InvalidParamError('password'))
+    })
+    
 })

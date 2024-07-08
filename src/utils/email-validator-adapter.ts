4dd1db760b0f947c7c90997684c015ac0735dc99
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-08 16:45:00 -0300

    feat: should return false as isValid

--- src/utils/email-validator-adapter.ts
+++ src/utils/email-validator-adapter.ts
@@ -1,8 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+ 
 import { IEmailValidator } from "../presentation/protocols/email-validator";
+import validator from 'validator'
 
 export default class EmailValidatorAdapter implements IEmailValidator {
     isValid(email: string): boolean {
-        return false
+        return validator.isEmail(email)
     }
 }

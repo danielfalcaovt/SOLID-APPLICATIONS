4cb3d54961b6a68acda97e714a60187e49e94b9e
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-08 14:12:46 -0300

    refactor: move all signup imports to a single file and remove emailvalidator import from generic protocols

---
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -0,0 +1,5 @@
+export * from "../../errors"
+export * from "../../helpers/http-helpers"
+export * from "../../protocols"
+export * from '../../protocols/email-validator'
+export * from "../../../domain/usecases/add-account"

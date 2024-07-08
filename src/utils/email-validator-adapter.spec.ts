a2a5540e4ae57159370e7c0d7fbdac0390a9e8b1
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-08 16:46:52 -0300

    test: should return false as isValid

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import EmailValidatorAdapter from "./email-validator-adapter"
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+    isEmail (): boolean {
+        return true
+    }
+}))
+
 
 describe('EmailValidator Adapter', () => {
     test('Should return false as isValid', () => {
         const sut = new EmailValidatorAdapter()
+        jest.spyOn(sut, 'isValid').mockReturnValueOnce(false)
         const isValid = sut.isValid('invalid_mail@mail.com')
         expect(isValid).toBe(false)
+    })
+    test('Should return true as isValid', () => {
+        const sut = new EmailValidatorAdapter()
+        const isValid = sut.isValid('valid_mail@mail.com')
+        expect(isValid).toBe(true)
     })
 })

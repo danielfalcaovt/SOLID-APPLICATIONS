921858b6c68bf4a675366d6fed972fd0e3db8edd
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-08 16:51:50 -0300

    test: should call isvalid with correct value

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -9,7 +9,6 @@ jest.mock('validator', () => ({
     }
 }))
 
-
 describe('EmailValidator Adapter', () => {
     test('Should return false as isValid', () => {
         const sut = new EmailValidatorAdapter()
@@ -21,5 +20,11 @@ describe('EmailValidator Adapter', () => {
         const sut = new EmailValidatorAdapter()
         const isValid = sut.isValid('valid_mail@mail.com')
         expect(isValid).toBe(true)
+    })
+    test('Should call isValid with correct value', () => {
+        const sut = new EmailValidatorAdapter()
+        const isValidSpy = jest.spyOn(sut, 'isValid')
+        sut.isValid('valid_mail@mail.com')
+        expect(isValidSpy).toHaveBeenCalledWith('valid_mail@mail.com')
     })
 })

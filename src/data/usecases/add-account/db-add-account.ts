c1a5c93e594c15cb16cbf883741b1076fcfa016e
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-09 15:10:33 -0300

    feat: ensure call addAccountRepository with correct valuees

---
+++ src/data/usecases/add-account/db-add-account.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import AccountModel from "../../../domain/models/account";
+import { AddAccountModel, AddAccountRepository, IEncrypter } from './db-add-account-protocols'
+
+export default class DbAddAccount implements AddAccountRepository {
+    private Encrypter : IEncrypter
+    private addAccountRepositoryStub: AddAccountRepository
+
+    constructor(encrypter: IEncrypter, AddAccountRepository: AddAccountRepository) {
+        this.Encrypter = encrypter
+        this.addAccountRepositoryStub = AddAccountRepository
+    }
+
+    async add(account: AddAccountModel): Promise<AccountModel> {
+        const { email, name, password } = account
+        const encryptedPassword = await this.Encrypter.encrypt(password)
+        const addedAccount = await this.addAccountRepositoryStub.add(Object.assign({}, account, { password: encryptedPassword }))
+        
+        
+        return new Promise(resolve => {
+            resolve(addedAccount)
+        })
+    }
+}

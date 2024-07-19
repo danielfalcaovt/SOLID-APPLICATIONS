6be1114eca163787ce50a538820dccfccd0f1223
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-19 15:09:26 -0300

    feat: ensure return 500 if emailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
-import { badRequest, IEmailValidator, InvalidParamError, MissingParamError, ok } from "../signup/signup-protocols";
+import { badRequest, IEmailValidator, InvalidParamError, MissingParamError, ok, serverError } from "../signup/signup-protocols";
 
 export class LoginController implements Controller {
     private readonly emailValidator: IEmailValidator
@@ -8,16 +9,23 @@ export class LoginController implements Controller {
         this.emailValidator = emailValidator
     }
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const requiredParameters = ['email', 'password']
-        for (const pos of requiredParameters) {
-            if (!httpRequest.body[pos]) {
-                return new Promise(resolve => resolve(badRequest(new MissingParamError(pos))))
+        try 
+        {
+            const requiredParameters = ['email', 'password']
+            for (const pos of requiredParameters) {
+                if (!httpRequest.body[pos]) {
+                    return new Promise(resolve => resolve(badRequest(new MissingParamError(pos))))
+                }
             }
+            const { email, password } = httpRequest.body
+            if (!this.emailValidator.isValid(email)) {
+                return new Promise(resolve => resolve(badRequest(new InvalidParamError('email'))))
+            }
+            return new Promise(resolve => resolve(ok('')))
+        }catch(error: any) {
+            return new Promise(resolve => {
+                resolve(serverError(error))
+            })
         }
-        const { email, password } = httpRequest.body
-        if (!this.emailValidator.isValid(email)) {
-            return new Promise(resolve => resolve(badRequest(new InvalidParamError('email'))))
-        }
-        return new Promise(resolve => resolve(ok('')))
     }
 }

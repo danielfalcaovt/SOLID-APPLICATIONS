714cde230b2f864639b94e22fab7ebc1dbf63b9b
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-19 14:05:09 -0300

    feat: ensure return 400 if no password was provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,12 +1,12 @@
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
-import { badRequest, InvalidParamError, ok } from "../signup/signup-protocols";
+import { badRequest, MissingParamError, ok } from "../signup/signup-protocols";
 
 export class LoginController implements Controller {
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         const requiredParameters = ['email', 'password']
         for (const pos of requiredParameters) {
             if (!httpRequest.body[pos]) {
-                return new Promise(resolve => resolve(badRequest(new InvalidParamError(pos))))
+                return new Promise(resolve => resolve(badRequest(new MissingParamError(pos))))
             }
         }
         return new Promise(resolve => resolve(ok('')))

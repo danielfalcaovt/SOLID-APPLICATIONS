b7e8a94ba1d1c3cedef0e7b082a6674bab904c92
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-23 15:23:06 -0300

    test: ensure ValidationComposite return the first error received

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -1,38 +1,51 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { MissingParamError } from "../../errors"
 import { RequiredFieldValidation } from "./required-fields-validation"
 import { IValidation } from "./validation"
 import { ValidationComposite } from "./validation-composite"
 
 interface SutTypes {
     sut: ValidationComposite
-    validationStub: IValidation
+    validations: IValidation[]
 }
 
 const makeSut = (): SutTypes => {
     const validations: IValidation[] = []
+    validations.push(makeValidationStub())
+    validations.push(makeValidationStub())
+    const sut = new ValidationComposite(validations)
+    return {
+        sut,
+        validations
+    }
+}
+
+const makeValidationStub = (): IValidation => {
     class ValidationStub implements IValidation {
         validate(data: any): Error | null {
             return null
         }
     }
     const validationStub = new ValidationStub()
-    validations.push(validationStub)
-    const sut = new ValidationComposite(validations)
-    return {
-        sut,
-        validationStub
-    }
+    return validationStub
 }
 
 describe('Validation Composite', () => {
-    it('Should return an error if any test fail', () => {
-        const { sut, validationStub } = makeSut()
-        jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+    it('Should return an error if any validation fail', () => {
+        const { sut, validations } = makeSut()
+        jest.spyOn(validations[0], 'validate').mockReturnValueOnce(new MissingParamError('any_param'))
         const response = sut.validate({ field: 'any_value' })
-        expect(response).toEqual(new Error())
+        expect(response).toEqual(new MissingParamError('any_param'))
     })
-    it('Should not return if no one test fail', () => {
+    it('Should return the first error received', () => {
+        const { sut, validations } = makeSut()
+        jest.spyOn(validations[0], 'validate').mockReturnValueOnce(new MissingParamError('first_error'))
+        jest.spyOn(validations[1], 'validate').mockReturnValueOnce(new MissingParamError('second_error'))
+        const error = sut.validate({ field: 'any_value' })
+        expect(error).toEqual(new MissingParamError('first_error'))
+    })
+    it('Should not return if no one validation fail', () => {
         const { sut } = makeSut()
         const response = sut.validate({data: 'any_data'})
         expect(response).toBeFalsy()

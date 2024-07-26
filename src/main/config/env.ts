142bee3a681f369bc39ca35b194ca7dc0920401e
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-26 18:51:20 -0300

    chore: docker-compose add package.json

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-    MongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017',
+    MongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017' /* || 'mongodb://127.0.0.1:27017' */,
     SECRET_KEY: 'deenedev@2023',
     Port: 3000
 }
